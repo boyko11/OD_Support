e8eaf7316e8496e76ffcd1e072a23d1cffa2192d
Boyko Todorov <boyko11@gmail.com>
2010-12-12 02:11:29 -0500

    system props and env vars, commented out not working top command

--- WebContent/groovyscripts/stats.groovy
+++ WebContent/groovyscripts/stats.groovy
@@ -3,6 +3,16 @@ html.pre('df -h'.execute().text)
 html.hr()
 html.h1("IP Config ('ifconfig')")
 html.pre('ifconfig'.execute().text)
+//html.hr()
+//html.h1("Top (top -n 1)")
+//def topCommand = ["sh", "-c", "top -n 1"].execute()
+//topCommand.waitFor()
+//topOutput = topCommand.text
+//println "Top Output Results: "
+//html.pre(topOutput)
 html.hr()
-html.h1("Top (top -n 1)")
-html.pre('top -n 1'.execute().text)
+html.h1("System Properties")
+System.properties.each{html.p(it)}
+html.hr()
+html.h1("Environment Variables")
+System.env.each{html.p(it)}
